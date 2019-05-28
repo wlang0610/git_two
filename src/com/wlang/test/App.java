b90a80e1042a564f4f1d59aeb7dab65186cd32ef
wlang <pengwei5969@163.com>
2019-05-28 14:06:17 +0800

    tianjia

--- src/com/wlang/test/App.java
+++ src/com/wlang/test/App.java
@@ -2,7 +2,7 @@ package com.wlang.test;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("哈哈哈");
-        System.out.println("嘿嘿");
+        System.out.println("hahha");
+        System.out.println("heihei");
     }
 }
